0e8d6b2e436471ce4d32ed3d5994b8bd654795ed
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2020-11-30 11:36:12 +0200

    leaders now attack player if they are directly on or around them

--- Task2-master/Task2-master/TAsk1 v.2/Form1.cs
+++ Task2-master/Task2-master/TAsk1 v.2/Form1.cs
@@ -449,7 +449,51 @@ namespace TAsk1_v._2
                             Map.enemeyArray[i, o].Attack(Map.Hero);
                         }
                     }
-                    
+                    if (Map.enemeyArray[i, o] != null && Map.enemeyArray[i, o].Symbol == " L")
+                    {
+                        //attacking anything above this object
+                        if (i + 1 == Map.Hero.X_coordinate && o == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //attacking anything below this object
+                        if (i - 1 == Map.Hero.X_coordinate && o == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //attacking anything to the right this object
+                        if (i == Map.Hero.X_coordinate && o + 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //attacking anything to the left this object
+                        if (i == Map.Hero.X_coordinate && o - 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //above and to the right
+                        if (i + 1 == Map.Hero.X_coordinate && o + 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //above and to the left
+                        if (i + 1 == Map.Hero.X_coordinate && o - 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //below and to the left
+                        if (i - 1 == Map.Hero.X_coordinate && o + 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                        //below and to the right
+                        if (i - 1 == Map.Hero.X_coordinate && o + 1 == Map.Hero.Y_coordinate)
+                        {
+                            Map.enemeyArray[i, o].Attack(Map.Hero);
+                        }
+                    }
+
+
                 }
             }
         }
