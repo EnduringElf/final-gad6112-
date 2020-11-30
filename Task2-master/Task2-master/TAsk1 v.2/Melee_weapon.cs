0bc2dc9df9be8e909bb6c9aa35790011e3e5b77c
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2020-11-30 04:21:00 +0200

    created the weapon and meele weapons classes with full fields and propetys in place not tested yet

---
+++ Task2-master/Task2-master/TAsk1 v.2/Melee_weapon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAsk1_v._2
+{
+    class Melee_weapon : weapon
+    {
+
+        public enum Type
+        {
+            Dagger, Longsword
+        }
+        public Melee_weapon(int X_coordinate, int Y_coordinate, char Symbol, Type weapon_type) : base(X_coordinate, Y_coordinate, Symbol)
+        {
+            if (weapon_type == Type.Dagger)
+            {
+                Weapon_type = "Dagger";
+                Durability = 10;
+                Damage = 3;
+                Cost = 3;
+                Range = 1;
+            }else if (weapon_type == Type.Longsword)
+            {
+                Weapon_type = "Longsword";
+                Durability = 6;
+                Damage = 4;
+                Cost = 5;
+                Range = 1;
+            }
+            
+
+        }
+        
+        
+        public override string ToString()
+        {
+            return " W";
+        }
+
+    }
+}
